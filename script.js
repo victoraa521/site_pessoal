30556ea2b225f9f8c86130b7e4538ef5e8c52281
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-06-03 09:41:43 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 
 var width = window.innerWidth;
 if(width < 930){
-    var botao = document.getElementById("bot1");
-
-    botao.addEventListener('click', function(){
+    function comum(){
         document.getElementById("uul").style.left = "-100%";
         document.getElementById("uul").style.transition=".3s"
+    }
+
+    var botao0 = document.getElementById("bot0");
+    botao0.addEventListener('click',function(){
+        comum()
+    })
+
+    var botao1 = document.getElementById("bot1");
+
+    botao1.addEventListener('click',function(){
+        comum()
     })
 
     var botao2 = document.getElementById("bot2");
 
     botao2.addEventListener('click', function(){
-        document.getElementById("uul").style.left = "-100%";
-        document.getElementById("uul").style.transition=".3s"
+        comum()
     })
 
     var botao3 = document.getElementById("bot3");
 
     botao3.addEventListener('click', function(){
-        document.getElementById("uul").style.left = "-100%";
-        document.getElementById("uul").style.transition=".3s"
+        comum()
     })
+
 
     var menu_lateral = document.getElementById("menu_lateral")
     menu_lateral.addEventListener('click', function(){
         if (document.getElementById("uul").style.left < "0px"){
             document.getElementById("uul").style.left = "0";
-            document.getElementById("uul").style.transition=".5s"
         }else{
             document.getElementById("uul").style.left = "-100%";
-            document.getElementById("uul").style.transition=".5s"
+            
         }
+        document.getElementById("uul").style.transition=".5s"
     })
 }
 
